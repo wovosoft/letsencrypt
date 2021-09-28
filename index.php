e811272c68fab4ae4f6d7c982810d019cf3e9fad
Narayan Adhikary <wovosoft@gmail.com>
2021-09-28 19:57:57 +0600

    dgdfg

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
-//$gk= new \AcmePhp\Core\src\GenerateKeyPair();
-//var_dump($gk->handle());
-$sk = new \AcmePhp\Core\src\SecureHttpClient();
-var_dump($sk->getClient());
+$client = new \AcmePhp\Core\src\Client();
+var_dump($client->register());
