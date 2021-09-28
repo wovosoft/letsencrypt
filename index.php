86a98d34c314d0d182532886001c0404dadacc1b
Narayan Adhikary <wovosoft@gmail.com>
2021-09-28 20:09:14 +0600

    sdf

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 $client = new \AcmePhp\Core\src\Client();
+//echo "<h1>Register</h1>";
+//print_r($client->register());
+
+
 echo "<h1>Register</h1>";
-print_r($client->register());
-echo "<h1>Register</h1>";
-print_r($client->authorize("wovo.xyz"));
+$options = $client->authorize("wovo.xyz");
+//$c=new AcmePhp\Core\Protocol\AuthorizationChallenge();
+//$c->get
+foreach ($options as $option) {
+    echo "Type :". $option->getType()."<br>";
+    echo "URL :". $option->getUrl()."<br>";
+    echo "Domain :". $option->getDomain()."<br>";
+    echo "Token :". $option->getToken()."<br>";
+    echo "Status :". $option->getStatus()."<br>";
+
+}
