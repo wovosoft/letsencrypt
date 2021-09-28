d2f5f1fea18be9290522a92168ce2fbf52542457
Narayan Adhikary <wovosoft@gmail.com>
2021-09-28 20:00:30 +0600

    sdfsdf

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 $client = new \AcmePhp\Core\src\Client();
+echo "<h1>Register</h1>";
 print_r($client->register());
+echo "<h1>Register</h1>";
+print_r($client->authorize("wovo.xyz"));
